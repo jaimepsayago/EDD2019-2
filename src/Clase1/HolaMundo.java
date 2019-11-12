2027d182722a4741c058a26ec9907d08f9c342fa
jaimepsayago <jaimepsayago@gmail.com>
2019-11-11 19:39:45 -0500

    printf y scanner commit

--- src/Clase1/HolaMundo.java
+++ src/Clase1/HolaMundo.java
@@ -1,10 +1,27 @@
 package Clase1; //paquete
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// linea de comando para imprimir
 		System.out.println("bienvenido a la programacion");
+		
+		//formato con printf
+		System.out.printf("%s \n %s \n", 
+		"bienvenido", "programación java");
+		
+		//Scanner 
+		Scanner input = new Scanner(System.in);
+		//mensaje de pedido de dastos
+		System.out.println("inserte un texto a continuacion:");
+		//toma el dato de la sig linea
+		String test = input.nextLine();
+		//imprimir dato
+		System.out.println(test);
+		//cerrar scanner
+		input.close();
 		
 	}
 	
