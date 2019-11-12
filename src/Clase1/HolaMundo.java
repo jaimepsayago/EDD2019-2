4f3ac6365472707fd617839e453bf17c6d576b0e
jaimepsayago <jaimepsayago@gmail.com>
2019-11-11 19:28:41 -0500

    hola mundo commit

---
+++ src/Clase1/HolaMundo.java
@@ -0,0 +1,13 @@
+package Clase1; //paquete
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// linea de comando para imprimir
+		System.out.println("bienvenido a la programacion");
+		
+	}
+	
+	//codota
+
+}
